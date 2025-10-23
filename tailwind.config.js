f8547e89280b45fdbdd1ed7be6c1360c62881ff5
Yaroslav <alinapavluhina@ukr.net>
2025-10-23 17:02:03 +0300

    add fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-// tailwind.config.js
 module.exports = {
-  darkMode: false, //вимкн темн тему
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
