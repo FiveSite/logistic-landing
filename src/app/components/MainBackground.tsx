832565b595403d06f22bd50b49cb9c9b7e4191e3
Yaroslav <alinapavluhina@ukr.net>
2025-11-04 13:08:59 +0200

    fix

--- src/app/components/MainBackground.tsx
+++ src/app/components/MainBackground.tsx
@@ -1,17 +1,18 @@
+import Head from 'next/head';
 import Image from 'next/image';
 
 export const MainBackground = () => {
   return (
     <div>
-      {' '}
+      <Head>
+        <link rel='preload' as='image' href='/images/main-bg.webp' />
+      </Head>
       <Image
-        rel='preload'
         src='/images/main-bg.webp'
         alt='main-bg'
         fill
         priority
         fetchPriority='high'
-        quality={85}
         sizes='100vw'
         className='object-cover object-center'
       />
